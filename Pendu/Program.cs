4a3b40a08edd075f83fca5b8c7859d6d90f1543f
Paul <strubpaul90@gmail.com>
2022-02-25 10:29:34 +0100

    Creating function to choose the character.

--- Pendu/Program.cs
+++ Pendu/Program.cs
@@ -27,7 +27,7 @@ namespace Pendu
                                     "emoucher",
                                     "logomachie",
                                     "boulingrin",
-                                    "hippopotomonstrosesquippedaliophobie"};
+                                    "hippopotomonstrosesuipedaliophobie"};
 
             string[] listAlcool =
             {
@@ -100,22 +100,33 @@ namespace Pendu
             bool InGame = false;
             string word = generateWords(int.Parse(lvl));
 
-            char [] LetterInWord = initTab(word);
+            char [] LetterInWord = initLetterInWord(word);
             char[] NotLetterInWord = initNotLetterTab(LetterInWord);
-
+            char[] PrintableTab = initPrintableTab(LetterInWord);
             while (InGame == false) //play until the player win/lose the game
             {
-                InGame = PrintGame(LetterInWord, NotLetterInWord, word,HP) ;
+                PrintableTab = PrintGame(LetterInWord, NotLetterInWord, word,HP,PrintableTab) ;
+                InGame = IfWin(PrintableTab);
             }
             
         }
 
-        static char [] initTab(string word)
+        static char [] initLetterInWord(string word)
         {
             char[] LetterInWord;
             LetterInWord = word.ToCharArray();
             return LetterInWord;
             
+        }
+
+        static char [] initPrintableTab(char [] LetterInWord)
+        {
+            char[] PrintableTab = new char[LetterInWord.Length];
+            for (int i = 0; i < LetterInWord.Length; i++)
+            {
+                PrintableTab[i] = '_';
+            }
+            return PrintableTab;
         }
 
         static char [] initNotLetterTab(char[] LetterInWord)
@@ -124,7 +135,7 @@ namespace Pendu
             int num = 26;
             for (int i = 0; i < LetterInWord.Length; i++)
             {
-                if (LetterInWord[i] == 0)
+                if (LetterInWord[i] == (char)0)
                 {
 
                 }
@@ -135,16 +146,113 @@ namespace Pendu
                     LetterInWord[i] = (char)0;
                     for (int a =0; a < LetterInWord.Length; a++)
                     {
-
+                        if (LetterInWord[a] == c)
+                        {
+                            LetterInWord[a] = (char) 0;
+                        }
                     }
                 }
+                
             }
+            char[] NotLetterInWord = new char[num];
             return NotLetterInWord;
         }
 
-        static bool PrintGame(char[] LetterInWord, char NotLetterInWord, string word, int Hp)
+        static char[] PrintGame(char[] LetterInWord, char[] NotLetterInWord, string word, int Hp, char[] PrintableTab) 
         {
+            PrintableTab = IfLetterRight(LetterInWord, PrintableTab,NotLetterInWord) ;
+            bool Pendu = PrintPendu(PrintableTab);
 
+        }
+
+        static char[] IfLetterRight(char[] LetterInWord, char [] PrintableTab,char[] NotLetterInWord) //Test if the char that th player give us is in the word 
+        {
+            Console.Clear();
+            Console.WriteLine("A toi de jouer donne nous une lettre !");
+            char CharChoose = Letter(PrintableTab,NotLetterInWord);
+            for (int i = 0; i < LetterInWord.Length; i++)
+            {
+                if (CharChoose == LetterInWord[i])
+                {
+                    PrintableTab[i] = CharChoose;
+
+                }
+            }
+            return PrintableTab;
+
+        }
+
+        static char Letter(char[] PrintableTab, char[] NotLetterInWord)
+        {
+            char[] List =
+            {
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z',
+            };
+            
+            try
+            {
+                char CharChooseS = Char.Parse(Console.ReadLine());
+                for (int a = 0; a < PrintableTab.Length; a++)
+                {
+                    if (CharChooseS == PrintableTab[a])
+                    {
+                        Console.WriteLine("Il nous faut une lettre que tu n'as pas déjà donné");
+                        return Letter(PrintableTab, NotLetterInWord);
+                    }
+                }
+                for (int i = 0; i<NotLetterInWord.Length; i++)
+                {
+                    if (CharChooseS == NotLetterInWord[i])
+                    {
+                        Console.WriteLine("Il nous faut une lettre que tu n'as pas déjà donné");
+                        return Letter(PrintableTab, NotLetterInWord); 
+                    }
+                }
+                
+                    for (int i = 0; i < List.Length; i++)
+                    {
+                        
+                        if (CharChooseS == List[i])
+                        {
+                        return CharChooseS;
+                        }
+                        
+                    }
+
+                Console.WriteLine("Il ne nous faut qu'une lettre en minuscule et rien d'autre");
+                return Letter(PrintableTab,NotLetterInWord);
+
+            }
+            catch
+            {
+                Console.WriteLine("Il ne nous faut qu'une lettre en minuscule et rien d'autre");
+                return Letter(PrintableTab,NotLetterInWord);
+            }
         }
         
     }
